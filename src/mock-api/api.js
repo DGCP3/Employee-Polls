b184f26f88d62ebf2c8840fd7d3a800e436f33a4
dgcp3 <zdagmawig1@gmail.com>
2022-05-22 19:09:35 +0300

    refactor: :recycle: refactor

--- src/mock-api/api.js
+++ src/mock-api/api.js
@@ -1,66 +1,46 @@
 export const url = "http://localhost:3001";
 
+const fetcher = async (url, method = "GET", data) => {
+  return await fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
+};
+
 export const __getUsers = async () => {
-  return await fetch(`${url}/users`).then((res) => res.json());
+  return await fetcher(`${url}/users`);
 };
 
 export const __getUser = async (id) => {
-  return await fetch(`${url}/users/${id}`).then((res) => res.json());
+  return await fetcher(`${url}/users/${id}`);
 };
 
 export const __getQuestions = async () => {
-  return await fetch(`${url}/questions`).then((res) => res.json());
+  return await fetcher(`${url}/questions`);
 };
 
 export const __getQuestion = async (id) => {
-  return await fetch(`${url}/questions/${id}`).then((res) => res.json());
+  return await fetcher(`${url}/questions/${id}`);
 };
 
 export const __postQuestion = async (question) => {
-  return await fetch(`${url}/questions`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(question),
-  }).then((res) => res.json());
+  return await fetcher(`${url}/questions`, "POST", question);
 };
 
 export const __postAnswer = async (answer) => {
-  return await fetch(`${url}/answers`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(answer),
-  }).then((res) => res.json());
+  return await fetcher(`${url}/answers`, "POST", answer);
 };
 
 export const __patchUser = async (id, data) => {
-  return await fetch(`${url}/users/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
+  return await fetcher(`${url}/users/${id}`, "PATCH", data);
 };
 export const __patchQuestion = async (id, data) => {
-  return await fetch(`${url}/questions/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
+  return await fetcher(`${url}/questions/${id}`, "PATCH", data);
 };
 
 export const __patchAnswer = async (id, data) => {
-  return await fetch(`${url}/answers/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).then((res) => res.json());
+  return await fetcher(`${url}/answers/${id}`, "PATCH", data);
 };
