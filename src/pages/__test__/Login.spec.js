6badf8990fd642f8c7947ab6eb2d847bd106bfce
dgcp3 <zdagmawig1@gmail.com>
2022-05-29 23:03:36 +0300

    test: :test_tube: test for login page

--- src/pages/__test__/Login.spec.js
+++ src/pages/__test__/Login.spec.js
@@ -3,24 +3,40 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { store } from "../../redux/store";
 import Login from "../Login";
+const TestLogin = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 afterEach(cleanup);
 describe("Login component", () => {
-  it("Login", () => {
-    const { container } = render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    );
-    // snapshot
+  it("should render", () => {
+    const { container } = render(<TestLogin />);
+    expect(container).toBeInTheDocument();
     expect(container).toMatchSnapshot();
+  });
+  it("should render the login form", () => {
+    render(<TestLogin />);
     expect(screen.getByTestId("loginForm")).toBeInTheDocument();
+  });
+  it("should render the username and password input", () => {
+    render(<TestLogin />);
     expect(screen.getByTestId("username")).toBeInTheDocument();
     expect(screen.getByTestId("password")).toBeInTheDocument();
+  });
+  it("should render the login button", () => {
+    render(<TestLogin />);
+    expect(screen.getByTestId("loginButton")).toBeInTheDocument();
+  });
+  it("should have correct text in input", () => {
+    render(<TestLogin />);
     expect(screen.getByTestId("loginForm")).toHaveFormValues({
       username: "zoshikanlu",
       password: "pass246",
