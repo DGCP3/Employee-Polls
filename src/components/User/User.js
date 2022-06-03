32c14406fca3f21802f3da8aeeedf6415b3d0d2b
dgcp3 <zdagmawig1@gmail.com>
2022-06-03 16:29:39 +0300

    feat: :sparkles: resusable component
    User component

---
+++ src/components/User/User.js
@@ -0,0 +1,25 @@
+import multiavatar from "@multiavatar/multiavatar";
+import { Container, Avatar, UserDetails, Flex } from "./style";
+// https://media0.giphy.com/media/LM7mVNy0iAZpTBAkIH/giphy.gif
+export default function User({ user }) {
+  return (
+    <Container>
+      <Avatar
+        src={`data:image/svg+xml;utf8,${encodeURIComponent(
+          multiavatar(user?.id)
+        )}`}
+      />
+      <h1>{user?.name}</h1>
+      <UserDetails>
+        <Flex>
+          <span>Answered :</span>
+          <span>{user?.answers}</span>
+        </Flex>
+        <Flex>
+          <span>Asked :</span>
+          <span>{user.questions}</span>
+        </Flex>
+      </UserDetails>
+    </Container>
+  );
+}
---
+++ src/components/User/style.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  font-family: "DM mono", monospace;
+  min-width: 250px;
+  margin: 10px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 10px;
+  background-color: #073154;
+  h1 {
+    color: #fff;
+    font-size: 1.5rem;
+    margin: 5px 0;
+  }
+`;
+const Avatar = styled.img`
+  width: 120px;
+  border-radius: 50%;
+  margin-block-end: 10px;
+`;
+const UserDetails = styled.p`
+  font-size: 1.2rem;
+  font-weight: 500;
+  width: 100%;
+  margin: 0 0;
+`;
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px;
+  padding: 0 0;
+`;
+export { Container, Avatar, UserDetails, Flex };
