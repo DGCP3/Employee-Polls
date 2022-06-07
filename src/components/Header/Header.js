64776a182713b824a2e1b584c51fadd0c1de01df
dgcp3 <zdagmawig1@gmail.com>
2022-06-07 18:14:44 +0300

    fix: remove logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Logo, StyledHeader } from "./styled";
-const defaultLogo = (
-  <img
-    src="https://cdn-icons.flaticon.com/png/512/2668/premium/2668751.png?token=exp=1654030864~hmac=7fa41121d92611d25efbe6bd7ab54ffd"
-    alt="Keep Logo"
-  />
-);
-export default function Header({
-  logo = defaultLogo,
-  title,
-  labelSize = "1.2rem",
-}) {
+import HeaderMenu from "../HeaderMenu";
+
+export default function Header({ logo, title, labelSize = "1.2rem" }) {
   return (
-    <>
-      <StyledHeader>
-        <Logo labelSize={labelSize}>
-          {logo}
-          <label>{title}</label>
-        </Logo>
-      </StyledHeader>
-    </>
+    <StyledHeader>
+      <Logo labelSize={labelSize}>
+        {logo}
+        <label>{title}</label>
+      </Logo>
+      <HeaderMenu />
+    </StyledHeader>
   );
 }
+Header.prototype = {
+  logo: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  labelSize: PropTypes.string,
+};
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   padding: 0 20px;
   height: 60px;
