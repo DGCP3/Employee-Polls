3e9ad5ac35abbf4d47f98d8dcd2dff727d17b5be
dgcp3 <zdagmawig1@gmail.com>
2022-06-07 16:33:31 +0300

    feat: :lipstick: add avatar option

--- src/components/Author/style.js
+++ src/components/Author/style.js
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 
 const AuthorStyle = styled.span`
+  display: flex;
+  align-items: center;
   font-size: ${(props) => props.fontSize || "0.8rem"};
   color: ${(props) => props.color || "#818181"};
   font-family: "DM Mono";
   font-weight: 500;
 `;
-export { AuthorStyle };
+const Avatar = styled.img`
+  width: ${(prop) => prop.width || "30px"};
+  border-radius: 50%;
+  display: inline;
+  margin-right: 10px;
+`;
+export { AuthorStyle, Avatar };
