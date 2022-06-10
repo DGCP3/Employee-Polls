977c9d2153adbd23d06f426cf632a1bc63898597
dgcp3 <zdagmawig1@gmail.com>
2022-06-10 18:50:30 +0300

    style: added after style

--- src/components/InputRadio/style.js
+++ src/components/InputRadio/style.js
@@ -17,7 +17,7 @@ const Label = styled.label`
   text-transform: capitalize;
   ${Radio}:checked + & {
     font-weight: bold;
-    font-size: 1.7rem;
+    font-size: 1.8rem;
   }
   ${Radio}:checked + &::before {
     content: "✔️";
@@ -25,6 +25,18 @@ const Label = styled.label`
     top: 50%;
     transform: translateY(-50%);
     left: 10px;
+    z-index: 999;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    height: 100%;
+    width: 25px;
+    border-left: 4px solid black;
+    background-color: ${(props) => props.optColor};
     z-index: 999;
   }
 `;
