9c69cc72284ca32a7fdbdd5b65f598ec307fdd51
dgcp3 <zdagmawig1@gmail.com>
2022-06-10 18:53:17 +0300

    style: login style

--- src/routes/LoginPage/styled.js
+++ src/routes/LoginPage/styled.js
@@ -28,3 +28,31 @@ export const Container = styled.div`
     text-underline-position: under;
   }
 `;
+
+const StyledSelect = styled.select`
+  width: 100%;
+  height: 40px;
+  border: none;
+  border-radius: 5px;
+  background-color: #ffffff;
+  font-size: 1.4rem;
+  font-weight: 500;
+  color: #042946;
+  &:focus {
+    outline: none;
+  }
+  & option {
+    background-color: #ffffff;
+    color: #042946;
+    padding-block: 10px;
+  }
+`;
+const SelectLabel = styled.label`
+  display: block;
+  font-size: 1.3rem;
+  font-weight: 500;
+  color: #ffffff;
+  margin-block: 10px;
+`;
+
+export { StyledSelect, SelectLabel };
