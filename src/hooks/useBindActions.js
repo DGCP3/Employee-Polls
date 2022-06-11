fa19602d85b6540babdc7e8ffe8579e075ff1c8f
dgcp3 <zdagmawig1@gmail.com>
2022-06-11 12:59:23 +0300

    perf: add memoization

--- src/hooks/useBindActions.js
+++ src/hooks/useBindActions.js
@@ -1,8 +1,12 @@
+import { useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 
 export default function useBindActions(actions) {
   const dispatch = useDispatch();
-  const methods = bindActionCreators(actions, dispatch);
+  const methods = useMemo(
+    () => bindActionCreators(actions, dispatch),
+    [actions, dispatch]
+  );
   return { ...methods };
 }
