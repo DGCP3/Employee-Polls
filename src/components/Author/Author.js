d6e1727816e3ed6e9d29b122fbd51a45bee3875e
dgcp3 <zdagmawig1@gmail.com>
2022-08-22 19:35:48 +0300

    format change

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,13 +1,13 @@
-import multiavatar from "@multiavatar/multiavatar";
-import PropTypes from "prop-types";
-import { AuthorStyle, Avatar } from "./style";
+import multiavatar from '@multiavatar/multiavatar'
+import PropTypes from 'prop-types'
+import { AuthorStyle, Avatar } from './style'
 
 export default function Author({ name, timestamp, color, fontSize, seed }) {
   return (
     <AuthorStyle color={color} fontSize={fontSize}>
       {seed && (
         <Avatar
-          fontSize={"0.1rem"}
+          fontSize={'0.1rem'}
           src={`data:image/svg+xml;utf8,${encodeURIComponent(
             multiavatar(seed)
           )}`}
@@ -15,11 +15,11 @@ export default function Author({ name, timestamp, color, fontSize, seed }) {
       )}
       @{name} at: {new Date(timestamp).toLocaleDateString()}
     </AuthorStyle>
-  );
+  )
 }
 Author.prototype = {
   name: PropTypes.string.isRequired,
   timestamp: PropTypes.string.isRequired,
   color: PropTypes.string,
   fontSize: PropTypes.string,
-};
+}
